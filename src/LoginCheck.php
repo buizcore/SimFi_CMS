b7c0d11bed6dedd0db8d1d6fba552e05464e0adb
Dominik Bonsch <dominik.bonsch@webfrap.net>
2014-08-06 18:54:04 +0200

    header

--- src/LoginCheck.php
+++ src/LoginCheck.php
@@ -1,6 +1,19 @@
 <?php
-
-
+/*******************************************************************************
+*
+* @author      : Dominik Bonsch <d.bonsch@buizcore.com>
+* @date        :
+* @copyright   : BuizCore GmbH <contact@buizcore.com>
+* @project     : BuizCore, The core business application plattform
+* @projectUrl  : http://buizcore.com
+*
+* @licence     : BSD License see: LICENCE/BSD Licence.txt
+*
+* @version: @package_version@  Revision: @package_revision@
+*
+* Changes:
+*
+*******************************************************************************/
 
 /**
  * @package com.BuizCore
